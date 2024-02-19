4af5a4ebb7982cf144e3838acb9e6ea6d0856f6d
David Tahim <davidtahim@gmail.com>
2024-02-19 14:04:23 -0300

    criação da classe texto

---
+++ src/main/java/com/invaders/base/Texto.java
@@ -0,0 +1,39 @@
+package com.invaders.base;
+
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+
+public class Texto extends Elemento {
+    private Font fonte;
+
+    public Texto() {
+        fonte = new Font("Tahoma", Font.PLAIN, 16);
+    }
+
+    public Texto (Font fonte) {
+        this.fonte = fonte;
+    }
+
+    public void desenha(Graphics2D g, String texto) {
+        desenha(g, texto,getPx(),getPy());
+    }
+
+    public void desenha(Graphics2D g, String texto,int px, int py){
+        if (getCor() != null) 
+            g.setColor(getCor());
+        
+       g.setFont(fonte);
+       g.drawString(texto, px, py);     
+        
+    }
+
+    public Font geFont() {
+        return fonte;
+    }
+
+    public void setFont(Font fonte ) {
+        this.fonte = fonte;    
+    }
+
+}
